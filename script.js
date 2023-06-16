1a67bbeb1d372e1490fa0b9b97d44738e8b5f2e5
Rango1996 <fahimmannan45@gmail.com>
2023-06-16 16:55:57 +0100

    JS update to allow user to pick 3 different colors

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let color = "black";
+
+//function to populate board with size inputted in using paramters entered in size field on HTML. 
+// also includes reset code to remove divs when colums/rows are redefined by user
 function populateBoard (size) {
     let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
@@ -5,15 +9,16 @@ function populateBoard (size) {
 board.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
 board.style.gridTemplateRows=`repeat(${size}, 1fr)`;
 
+
 let amount = size*size;
 for (let i=0; i<amount; i++) {
     let square = document.createElement("div");
-    square.addEventListener("mouseover", () =>
-    {square.style.backgroundColor="black"});  
+    square.addEventListener("mouseover", colorSquare) 
     square.style.backgroundColor="grey";
     board.insertAdjacentElement("beforeend", square);
 }  
 }
+// limit inputs of at least 2 and max of 100
 
 function changeSize(input) {
         if (input >= 2 && input <=100) {
@@ -22,6 +27,14 @@ function changeSize(input) {
                 console.log("issues")
             }
         }
+// color square function defined as background style of div that's created initially = color of button
+function colorSquare() {
+    this.style.backgroundColor=color;
+    }
+
+function changeColor(choice) {
+    color=choice;
+}
 
 
 populateBoard(16);
